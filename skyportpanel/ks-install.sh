bfb16bc48d3333f3f04f77781e3d75a57820b877
KSWarrior <kswarriorpro@gmail.com>
2025-06-01 22:24:33 +0530

    Update ks-install.sh

--- skyportpanel/ks-install.sh
+++ skyportpanel/ks-install.sh
@@ -9,17 +9,17 @@ read -p "Enter your choice [1-3]: " choice
 case $choice in
     1)
         echo "Installing Panel..."
-        bash <(curl -sSL https://example.com/install-panel.sh)
+        bash <(curl -sSL https://raw.githubusercontent.com/KSWarrior/moh-codes/refs/heads/main/skyportpanel/panel.sh)
         ;;
     2)
         echo "Installing Wings..."
-        bash <(curl -sSL https://example.com/install-wings.sh)
+        bash <(curl -sSL https://raw.githubusercontent.com/KSWarrior/moh-codes/refs/heads/main/skyportpanel/wings.sh)
         ;;
     3)
         echo "Installing Panel..."
-        bash <(curl -sSL https://example.com/install-panel.sh)
+        bash <(curl -sSL https://raw.githubusercontent.com/KSWarrior/moh-codes/refs/heads/main/skyportpanel/panel.sh)
         echo "Installing Wings..."
-        bash <(curl -sSL https://example.com/install-wings.sh)
+        bash <(curl -sSL https://raw.githubusercontent.com/KSWarrior/moh-codes/refs/heads/main/skyportpanel/wings.sh)
         ;;
     *)
         echo "Invalid option. Please enter 1, 2, or 3."
