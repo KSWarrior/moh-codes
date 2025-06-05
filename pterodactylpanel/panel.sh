528a49cd15c9faa928eb99a81fca7cfa047e3e6a
KSWarrior <kswarriorpro@gmail.com>
2025-06-05 08:08:44 +0530

    Update panel.sh

--- pterodactylpanel/panel.sh
+++ pterodactylpanel/panel.sh
@@ -7,8 +7,8 @@ echo "   💬 Join our Discord: https://discord.gg/2kAYnH655h"
 echo "=============================================="
 
 # Create and enter panel directory
-mkdir -p pterodactyl/panel
-cd ~/pterodactyl/panel
+mkdir pterodactyl/panel
+cd pterodactyl/panel
 
 # Download panel.yml as docker-compose.yml
 wget -q https://raw.githubusercontent.com/KSWarrior/moh-codes/refs/heads/main/pterodactylpanel/panel.yml -O docker-compose.yml
