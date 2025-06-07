b0470f02e4be023e8412aa65d0aacbcb5cb8ff17
KSWarrior <kswarriorpro@gmail.com>
2025-06-07 09:28:58 +0530

    Update ks-install.sh

--- pufferpanel/ks-install.sh
+++ pufferpanel/ks-install.sh
@@ -6,13 +6,17 @@ echo "   💬 Join our Discord: https://discord.gg/2kAYnH655h"
 echo "=============================================="
 
 # Add the PufferPanel APT repository and its GPG key
-curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash
+echo "Puffer Panel Download ..."
+curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | bash  > ks-puffer.log
 
 # Install PufferPanel without prompts
-apt-get install -y pufferpanel
+echo "Puffer Panel installing ..."
+apt-get install -y pufferpanel > ks-puffer.log
 
 # Add admin user (interactive)
+echo "Create User for Puffer Panel"
 pufferpanel user add
+echo "{}" > email/emails.json
 
 # Start PufferPanel
 pufferpanel run
